1434186a3edf987dfa75fa7b7a2b542ebe00e6e0
tirth-2001 <tirthgpatel.27@gmail.com>
2021-09-19 09:32:35 +0530

    One Signal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,39 @@ import React from 'react';
 import {View, Text, StyleSheet, Button, StatusBar} from 'react-native';
 import Routes from './Routes';
 
+import OneSignal from 'react-native-onesignal';
+
+//OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId('6504f9bd-9178-4642-a552-fc57346ace55');
+//END OneSignal Init Code
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log('Prompt response:', response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(
+  notificationReceivedEvent => {
+    console.log(
+      'OneSignal: notification will show in foreground:',
+      notificationReceivedEvent,
+    );
+    let notification = notificationReceivedEvent.getNotification();
+    console.log('notification: ', notification);
+    const data = notification.additionalData;
+    console.log('additionalData: ', data);
+    // Complete with null means don't show a notification.
+    notificationReceivedEvent.complete(notification);
+  },
+);
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log('OneSignal: notification opened:', notification);
+});
+
 const App = () => {
   return (
     <>
